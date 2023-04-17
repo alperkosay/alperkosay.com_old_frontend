ce36d230b6727b196a3d09319a251b364aa15d95
Alper Koşay <83144508+Awoked@users.noreply.github.com>
2023-04-17 19:35:09 +0300

    update

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,19 +5,31 @@ import SkillsSection from '@/components/SkillsSection';
 import AboutSection from '@/components/AboutSection';
 import DrawingsSection from '@/components/DrawingsSection';
 import ProjectsSection from '@/components/ProjectsSection';
+import Head from 'next/head';
 
 const chivo_mono = Chivo_Mono({ subsets: ['latin'] })
 
 export default function Home() {
 
   return (
-    <main className={`${chivo_mono.className} transition-all`}>
 
-      <HeroSection />
-      <AboutSection />
-      <SkillsSection />
-      <ProjectsSection />
-      <DrawingsSection />
-    </main>
+    <>
+      <Head>
+
+        <title>Alper Koşay</title>
+        <meta name="description" content="Kişisel portfolyo websitem." />
+        <meta name="keywords" content="Alper Koşay, Web Developer, React Developer" />
+        <meta name='author' content='Alper Koşay' />
+        
+      </Head>
+      <main className={`${chivo_mono.className} transition-all`}>
+
+        <HeroSection />
+        <AboutSection />
+        <SkillsSection />
+        <ProjectsSection />
+        <DrawingsSection />
+      </main>
+    </>
   )
 }
