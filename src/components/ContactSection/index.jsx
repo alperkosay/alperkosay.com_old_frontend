aa78a046d3ee210c8647377925dd99d07f31024b
Alper Koşay <83144508+Awoked@users.noreply.github.com>
2023-04-18 01:28:47 +0300

    update

---
+++ src/components/ContactSection/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ContactSection = () => {
+    return (
+        <section className='contact-section'>
+            
+
+        </section>
+    )
+}
+
+export default ContactSection
