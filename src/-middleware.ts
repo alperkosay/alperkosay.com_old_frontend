03dfae7bfcc6c52bcfcb098eeb867d950d6afa1c
Alper Koşay <83144508+Awoked@users.noreply.github.com>
2023-10-16 10:32:21 +0300

    update

---
+++ src/-middleware.ts
@@ -0,0 +1,21 @@
+
+
+// import { withAuth } from "next-auth/middleware";
+// import { NextResponse } from "next/server";
+
+
+// export default withAuth(
+//     function middleware(req) {
+//     },
+//     {
+//         callbacks: {
+//             authorized({ token }) {
+//                 return token?.role === "admin"
+//             },
+//         }
+//     }
+// )
+
+// export const config = {
+//     matcher: ["/dashboard", "/dashboard/:path*"]
+// }
